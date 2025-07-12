e917708a3c2bf05f50c712aba6aa353f05c646f9
PovedaAqui <luis.poveda9321@gmail.com>
2025-07-13 00:36:25 +0200

    minor error updated

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingIncludes: {
-      '/': ['./lib/db/migrations/**/*']
-    }
+  outputFileTracingIncludes: {
+    '/': ['./lib/db/migrations/**/*']
   }
 };
 
